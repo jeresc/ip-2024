c5ce19a5d616830b3a8be8a741e335092200a928
jeresc <jeremiastomassrc@gmail.com>
2024-09-08 15:18:52 -0300

    chore: guia 4 hasta ejercicio 13 completa

--- guias/guia3.hs
+++ guias/guia3.hs
@@ -4,13 +4,13 @@ parcialF 1 = 8
 parcialF 4 = 131
 parcialF 16 = 16
 
--- b)
+-- 1.b)
 parcialG :: Integer -> Integer
 parcialG n | n == 8 = 16
            | n == 16 = 4
            | n == 131 = 1
 
--- c)
+-- 1.c)
 parcialH :: Integer -> Integer
 parcialH = parcialF . parcialG
 -- parcialH n = parcialF (parcialG n)
@@ -24,18 +24,18 @@ absoluto :: Integer -> Integer
 absoluto n | n >= 0 = n
            | otherwise = -n
 
--- b)
+-- 2.b)
 maximoAbsoluto :: Integer -> Integer -> Integer
 maximoAbsoluto x y | abs x > abs y = abs x
                    | otherwise = abs y
 
--- c)
+-- 2.c)
 maximo3 :: Integer -> Integer -> Integer -> Integer
 maximo3 x y z | x > y && x > z = x
               | y > z = y
               | otherwise = z
 
--- d) Sin pattern matching
+-- 2.d) Sin pattern matching
 algunoEs0 :: Float -> Float -> Bool
 algunoEs0 x y = x == 0 || y == 0
 
@@ -45,11 +45,11 @@ algunoEs02 _ 0 = True
 algunoEs02 0 _ = True
 algunoEs02 _ _ = False
 
--- f) (-Inf, 3), (3, 7], (7, Inf)
+-- 2.f) (-Inf, 3), (3, 7], (7, Inf)
 mismoIntervalo :: Float -> Float -> Bool
 mismoIntervalo x y = (x <= 3 && y <= 3) || (x > 7 && y > 7) || (x <= 7 && y <= 7 && x > 3 && y > 3)
 
--- g)
+-- 2.g)
 sumaDistintos :: Integer -> Integer -> Integer -> Integer
 sumaDistintos x y z | x == y && x == z = x
                     | x == y && x /= z = x + z
@@ -57,15 +57,15 @@ sumaDistintos x y z | x == y && x == z = x
                     | y == z && y /= x = y + x
                     | otherwise = x + y + z
 
--- h)
+-- 2.h)
 esMultiploDe :: Integer -> Integer -> Bool
 esMultiploDe x y = x `mod` y == 0
 
--- i)
+-- 2.i)
 digitoUnidades :: Integer -> Integer
 digitoUnidades x = x `mod` 10
 
--- j)
+-- 2.j)
 digitoDecenas :: Integer -> Integer
 digitoDecenas x = x `mod` 100
 
@@ -78,20 +78,20 @@ type Punto2D = (Float, Float)
 prodInt :: Punto2D -> Punto2D -> Float
 prodInt (x1, y1) (x2, y2) = x1 * x2 + y1 * y2
 
--- b)
+-- 4.b)
 todoMenor :: Punto2D -> Punto2D -> Bool
 todoMenor (x1, y1) (x2, y2) = x1 < x2 && y1 < y2
 
--- c)
+-- 4.c)
 distanciaPuntos :: Punto2D -> Punto2D -> Float
 distanciaPuntos (x1, y1) (x2, y2) = sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
 
--- d)
+-- 4.d)
 type Terna = (Integer, Integer, Integer)
 sumaTerna :: Terna -> Integer
 sumaTerna (x,y,z) = x + y + z
 
--- e)
+-- 4.e)
 sumarSoloMultiplos :: Terna -> Integer -> Integer
 sumarSoloMultiplos (x,y,z) n | x `mod` n == 0 && y `mod` n == 0 && z `mod` n == 0 = x + y + z
                              | x `mod` n == 0 && y `mod` n == 0 = x + y
@@ -104,7 +104,7 @@ sumarSoloMultiplos (x,y,z) n | x `mod` n == 0 && y `mod` n == 0 && z `mod` n == 
 -- sumarSoloMultiplos (a, b, c) d = sum [x | x <- [a, b, c], x `mod` d == 0]
 
 
--- f)
+-- 4.f)
 posPrimerPar :: Terna -> Integer
 -- even o bien n `mod` 2 == 0
 posPrimerPar (x,y,z) | even x = 0
@@ -112,11 +112,11 @@ posPrimerPar (x,y,z) | even x = 0
                      | even z = 2
                      | otherwise = 4
 
--- g)
+-- 4.g)
 crearPar :: a -> b -> (a,b)
 crearPar a b = (a,b)
 
--- h)
+-- 4.h)
 invertir :: (a,b) -> (b,a)
 invertir (a,b) = (b,a)
 
--- guias/guia4.hs
+++ guias/guia4.hs
@@ -29,3 +29,86 @@ medioFact 0 = 1
 medioFact 1 = 1
 medioFact n = n * medioFact (n-2)
 
+-- 6)
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n = n < 10 || (ultimoDigito n == ultimoDigito (sinUltimoDigito n) && todosDigitosIguales (sinUltimoDigito n))
+
+ultimoDigito:: Integer -> Integer
+ultimoDigito n = mod n 10
+
+sinUltimoDigito:: Integer -> Integer
+sinUltimoDigito n = div n 10
+
+-- 7)
+iesimoDigito :: Integer -> Integer -> Integer
+iesimoDigito n i | cantDigitos n == i = ultimoDigito n
+                 | otherwise = iesimoDigito (sinUltimoDigito n) i
+
+cantDigitos :: Integer -> Integer
+cantDigitos n | n < 10 = 1
+              | otherwise = 1 + cantDigitos (div n 10)
+
+-- 8)
+sumaDigitos :: Integer -> Integer
+sumaDigitos 0 = 0
+sumaDigitos n = mod n 10 + sumaDigitos (div n 10)
+
+-- 9)
+esCapicua :: Integer -> Bool
+esCapicua n = n == invertirNumero n 0
+
+invertirNumero :: Integer -> Integer -> Integer
+invertirNumero 0 invertido = invertido
+invertirNumero n invertido = invertirNumero (n `div` 10) (invertido * 10 + n `mod` 10)
+
+-- 10.a)
+f1 :: Integer -> Integer
+f1 0 = 1
+f1 n = 2^n + f1 (n-1)
+
+-- 10.b)
+f2 :: Integer -> Integer -> Integer
+f2 1 q = q
+f2 n q = q^n + f2 (n - 1) q
+
+-- 10.c)
+f3 :: Integer -> Integer -> Integer
+f3 n 1 = 2 * n
+f3 1 q = f2 2 q
+f3 n q = f2 (2 * n) q
+
+-- 10.d)
+f4 :: Integer -> Integer -> Integer
+f4 n q = f3 n q - f2 (n - 1) q
+
+-- 11.a)
+eAprox :: Integer -> Float
+eAprox 0 = 1
+eAprox n = 1 / fromIntegral (factorial n) + eAprox (n-1)
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+-- 11.b)
+e :: Float
+e = eAprox 10
+
+-- 12)
+raizDe2Aprox :: Integer -> Float
+raizDe2Aprox 1 = 1
+raizDe2Aprox n = 1 + 1 / raizDe2AproxAux (n - 1)
+
+raizDe2AproxAux :: Integer -> Float
+raizDe2AproxAux 1 = 2
+raizDe2AproxAux n = 2 + 1 / raizDe2AproxAux (n - 1)
+
+-- 13)
+sumaDoble :: Integer -> Integer -> Integer
+sumaDoble 0 m = 0
+sumaDoble n m = sumaInterior n m + sumaDoble (n - 1) m
+
+sumaInterior :: Integer -> Integer -> Integer
+sumaInterior n 0 = 0
+sumaInterior n m = n ^ m + sumaInterior n (m - 1)
+
