6de09b3465e6d637cf7eb185e4b57f87676b02ad
jeresc <jeremiastomassrc@gmail.com>
2024-09-23 22:22:05 -0300

    feat: parcial 2023 2do cuatrimestre 1 resuelto

--- parciales/haskell/20232c_2.hs
+++ parciales/haskell/20232c_2.hs
@@ -1,86 +1,91 @@
 {-
-Introducción a la Programación
-Primer Parcial - Turno Mañana
-* El parcial se aprueba con 6 puntos
-* Podrás utilizar las siguientes funciones del prelude
-   * Listas: head, tail, last, init, length, elem, ++
-   * Tuplas: fst, snd
-   * Operaciones lógicas: &&, ||, not
-   * Constructores de listas: (x:xs), []
-   * Constructores de tuplas: (x,y)
-* Si querés utilizar Hunit para testear tu código acá tenés un script de ejemplo.
-Viva la democracia:
-La elección periódica de los gobernantes es la base de los Estados Modernos. Este sistema, denominado "democracia" (término proveniente de la antigua Grecia), tiene diferentes variaciones, que incluyen diferentes formas de elección del/a máximo/a mandatario/a. Por ejemplo, en algunos países se eligen representantes en un colegio electoral (EEUU). En otros se vota a los/as miembros del parlamento (España). En nuestro país elegimos de forma directa la fórmula presidencial (Presidente/a y Vicepresidente/a) cada 4 años.
-A continuación presentamos una serie de ejercicios que tienen como objetivo implementar funciones para sistema de escrutinio de una elección presidencial. Leer las descripciones y especificaciones e implementar las funciones requeridas en Haskell, utilizado sóĺamente las herramientas vistas en clase.
-Las fórmulas presidenciales serán representadas por tuplas (String x String), donde la primera componente será el nombre del candidato a presidente, y la segunda componente será el nombre del candidato a vicepresidente.
-En los problemas en los cuales se reciban como parámetro secuencias de fórmulas y votos, cada posición de la lista votos representará la cantidad de votos obtenidos por la fórmula del parámetro formulas en esa misma posición. Por ejemplo, si la lista de fórmulas es [("Juan Pérez","Susana García"), ("María Montero","Pablo Moreno")] y la lista de votos fuera [34, 56], eso indicaría que la fórmula encabezada por María Montero obtuvo 56 votos, y la lista encabezada por Juan Pérez obtuvo 34 votos.
-________________
+
+¡Vamos Campeones!
+En exactas se está jugando un torneo de fútbol y la facultad le pidió a los alumnos de IP programar algunas
+funcionalidades en Haskell, Los datos con los que contamos para esto son los nombres de los equipos que participan
+del torneo, los nombres de los arqueros titulares de cada uno de dichos equipos, y la cantidad de goles recibidos
+por esos arqueros. Los nombres de los equipos y sus respectivos arqueros serán modelados mediante tuplas de tipo
+(String, String), donde la primera componente representa el nombre del equipo, y la segunda representa el nombre
+del arquero titular de dicho equipo.
+En los problemas en los cuales se reciben como parámetros secuencias arquerosPorEquipo y goles, cada posición de
+la lista goles representará la cantidad de goles recibidos por el arquero del equipo que se encuentra en esa misma
+posicion de arquerosPorEquipo. Por ejemplo, si la lista arquerosPorEquipo es [("Sacachispas", "Neyder Aragon"),
+("Fenix", "Nahuel Galardi")] y la lista de goleses [3, 5], eso indicaría que Neyder Aragon recibió 3 goles, y
+Nahuel Galardi 5.
+
+Se pueden usar las siguientes funciones del preludio:
+	- Listas: head, tail, last, init, length, elem, ++
+	- Tuplas: fst, snd
+	- Operaciones Lógicas: &&, ||, not
+	- Constructores de listas: (x:xs), []
+	- Constructores de tuplas: (x, y)
 
 
-1) Porcentaje de Votos Afirmativos [1 punto]
-problema porcentajeDeVotosAfirmativos (formulas: seq⟨String x String⟩,votos:seq< Z >, cantTotalVotos: Z) : R {
- requiere: {¬formulasInvalidas(formulas)}
- requiere: {|formulas| = |votos|}
- requiere: {Todos los elementos de votos son mayores o iguales a 0}
- requiere: {La suma de todos los elementos de votos es menor o igual a cantTotalVotos}
- asegura: {res es el porcentaje de votos no blancos (es decir, asociados a alguna de las fórmulas) sobre el total de votos emitidos}
+1) Atajaron Suplentes
+problema atajaronSuplentes (arquerosPorEquipo: seq<String X String>, goles: seq<Z>, totalGolesTorneo: Z): Z {
+	requiere: {equiposValidos(arquerosPorEquipo)
+	requiere: {|arquerosPorEquipo| = |goles|}
+	requiere: {Todos los elementos de goles son mayores o iguales a 0}
+	requiere: {La suma de todos los elementos de goles es menor o igual a totalGolesTorneo}
+	asegura: {
+	res es la cantidad de goles recibidos en el torneo por arqueros que no son titulares en sus equipos.
+	}
 }
-Para resolver este ejercicio pueden utilizar la siguiente función que devuelve como Float la división entre dos números de tipo Int:
 
+
+2) Equipos Válidos
+problema equiposValidos (arquerosPorEquipo: seq<String X String>): Bool {
+	requiere: {True}
+	asegura: {
+	(res = True) <=> arquerosPorEquipo no contiene nombres de clubes repetidos, ni arqueros repetidos, ni jugadores con nombre del club
+	}
+}
+
+
+3) Porcentaje de goles
+problema porcentajeDeGoles (arquero: String, arquerosPorEquipo: seq<String X String>, goles: seq<Z>): R {
+	requiere: {La segunda componente de algún elemento de arquerosPorEquipo es arquero}
+	requiere: {equiposValidos(arquerosPorEquipo)}
+	requiere: {|arquerosPorEquipo| = |goles|}
+	requiere: {Todos los elementos de goles son mayores o iguales a 0}
+	requiere: {Hay al menos un elemento de goles mayores estricto a 0}
+	asegura: {
+	res es el porcentaje de goles que recibió arquero sobre el total de goles recibidos por arqueros titulares
+	}
+}
+
+Para resolver este ejercicio pueden utilizar la siguiente función que devuelve como float la división entre dos
+numeros de tipo Int.
 
 division :: Int -> Int -> Float
-division a b = (fromIntegral a) / (fromIntegral b)
-________________
+division a b = fromIntegral a / fromIntegral b
 
 
-2) Formulas Inválidas [3 puntos]
-problema formulasInvalidas (formulas: seq⟨String x String⟩) : Bool {
- requiere: {True}
- asegura: {(res = true) <=> formulas contiene un candidato se propone para presidente y vicepresidente en la misma fórmula; o algún candidato se postula para presidente o vice en más de una fórmula }
-________________
-
-
-3) Porcentaje de Votos [3 puntos]
-problema porcentajeDeVotos (vice: String, formulas: seq⟨String x String⟩,votos:seq< Z >) : R {
- requiere: {La segunda componente de algún elemento de formulas es vice}
- requiere: {¬formulasInvalidas(formulas)}
- requiere: {|formulas| = |votos|}
- requiere: {Todos los elementos de votos son mayores o iguales a 0}
- requiere: {Hay al menos un elemento de votos mayores estricto a 0}
- asegura: {res es el porcentaje de votos que obtuvo vice sobre el total de votos afirmativos}
+4) Valla Menos Vencida
+problema vallaMenosVencida (arquerosPorEquipo: seq<String X String>, goles: seq<Z>): String {
+	requiere: {equiposValidos(arquerosPorEquipo)}
+	requiere: {|arquerosPorEquipo| = |goles|}
+	requiere: {Todos los elementos de goles son mayores o iguales a 0}
+	requiere: {|goles| > 0}
+	asegura: {
+	res es alguno de los arqueros de arquerosPorEquipo que menor goles recibió de acuerdo a goles
+	}
 }
-Para resolver este ejercicio pueden utilizar la función division presentada en el Ejercicio 1.
-________________
 
-
-4) Menos Votado [3 puntos]
-problema menosVotado (formulas: seq⟨String x String⟩, votos:seq< Z >) : String {
- requiere: {¬formulasInvalidas(formulas)}
- requiere: {|formulas| = |votos|}
- requiere: {Todos los elementos de votos son mayores o iguales a 0}
- requiere: {Hay al menos un elemento de votos mayores estricto a 0}
- requiere: {|formulas| > 0}
- asegura: {res es el candidato a presidente de formulas menos votado de acuerdo a los votos contabilizados en votos}
-}
-A continuación te dejamos una estructura básica para resolver los ejercicios. Este código no pretende resolver ningun caso de los ejercicios planteados, es sólo una plantilla.
-
---}
+-}
 
 -- Ejercicio 1
-porcentajeDeVotosAfirmativos :: [(String, String)] -> [Int] -> Int  -> Float
-porcentajeDeVotosAfirmativos formulas votos total = 0
-
+atajaronSuplentes :: [(String, String)] -> [Int] -> Int -> Int
+atajaronSuplentes _ _ _ = 0
 
 -- Ejercicio 2
-formulasInvalidas :: [(String, String)] -> Bool
-formulasInvalidas _ = True
-
+equiposValidos :: [(String, String)] -> Bool
+equiposValidos _ = True
 
 -- Ejercicio 3
-porcentajeDeVotos :: String -> [(String, String)] -> [Int] -> Float
-porcentajeDeVotos _ _ _ = 0.0
-
+porcentajeDeGoles :: String -> [(String, String)] -> [Int] -> Float
+porcentajeDeGoles _ _ _ = 0
 
 -- Ejercicio 4
-menosVotado :: [(String, String)] -> [Int] -> String
-menosVotado _ _ = ""
+vallaMenosVencida :: [(String, String)] -> [Int] -> String
+vallaMenosVencida _ _ = ""
